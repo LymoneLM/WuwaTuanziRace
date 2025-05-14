09dc0c1672b543f479284920273bdd6c91a4d29e
LymoneLM <chengziorange233@qq.com>
2025-05-15 02:30:19 +0800

    fix: 格式化最终胜率输出

--- main.py
+++ main.py
@@ -9,10 +9,10 @@ if __name__ == "__main__":
                    tuanzi.carlotta,
                    ]
     score_list = [0 for i in range(len(tuanzi_list))]
-    num = 100000
+    num = 10000
     for i in range(num):
         race_map = tuanzi.RaceMap(tuanzi_list)
         score_list[race_map.run_match()] += 1
         print(score_list)
     for i in range(len(tuanzi_list)):
-        print(f"{tuanzi_list[i].name}:{score_list[i] / num:.2%}")
+        print(f"{tuanzi_list[i].name:<4}\t: {score_list[i] / num:.2%}")
