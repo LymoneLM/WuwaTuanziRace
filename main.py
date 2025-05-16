489e55ab17e32f745df846917d6395c6aa951053
LymoneLM <chengziorange233@qq.com>
2025-05-16 14:32:24 +0800

    feat: 新增day3模拟

--- main.py
+++ main.py
@@ -1,5 +1,23 @@
 import tuanzi
 
+
+rounds = 100000
+
+def run(tuanzi_list, **kw):
+    rank = None
+    if "rank" in kw:
+        rank = kw["rank"]
+    length = 23
+    if length in kw:
+        length = kw["length"]
+    score_list = [0 for _ in range(len(tuanzi_list))]
+    for i in range(rounds):
+        race_map = tuanzi.RaceMap(tuanzi_list, rank, length)
+        score_list[race_map.run_match()] += 1
+        print(score_list)
+    for i in range(len(tuanzi_list)):
+        print(f"{tuanzi_list[i].name:<4}\t: {score_list[i] / rounds:.2%}")
+
 def day1():
     tuanzi_list = [
         tuanzi.jinhsi,
@@ -9,14 +27,8 @@ def day1():
         tuanzi.camellya,
         tuanzi.carlotta,
     ]
-    score_list = [0 for _ in range(len(tuanzi_list))]
-    num = 100000
-    for i in range(num):
-        race_map = tuanzi.RaceMap(tuanzi_list)
-        score_list[race_map.run_match()] += 1
-        print(score_list)
-    for i in range(len(tuanzi_list)):
-        print(f"{tuanzi_list[i].name:<4}\t: {score_list[i] / num:.2%}")
+    run(tuanzi_list)
+
 
 def day2():
     tuanzi_list = [
@@ -27,16 +39,21 @@ def day2():
         tuanzi.carlotta,
         tuanzi.calcharo,
     ]
-    rank = [0,1,1,2,2,3]
-    score_list = [0 for _ in range(len(tuanzi_list))]
-    num = 100000
-    for i in range(num):
-        race_map = tuanzi.RaceMap(tuanzi_list, rank, 23+4)
-        score_list[race_map.run_match()] += 1
-        print(score_list)
-    for i in range(len(tuanzi_list)):
-        print(f"{tuanzi_list[i].name:<4}\t: {score_list[i] / num:.2%}")
+    rank = [0, 1, 1, 2, 2, 3]
+    run(tuanzi_list, rank=rank, length=23+4)
+
+
+def day3():
+    tuanzi_list = [
+        tuanzi.roccia,
+        tuanzi.brant,
+        tuanzi.cantarella,
+        tuanzi.zani,
+        tuanzi.cartethyia,
+        tuanzi.phoebe,
+    ]
+    run(tuanzi_list)
+
 
 if __name__ == "__main__":
-    day2()
-
+    day3()
